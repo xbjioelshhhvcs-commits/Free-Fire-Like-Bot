76cfd283d5f489491d10d7ca0a4eca9b4e2f2557
xbjioelshhhvcs-commits <xbjioelshhhvcs@gmail.com>
2026-06-03 00:33:05 +0300

    Update main.py
    fffddd

--- main.py
+++ main.py
@@ -28,11 +28,10 @@ if not BOT_TOKEN:
     logger.error("❌ BOT_TOKEN not found! Please set your bot token in environment variables.")
     sys.exit(1)
 
-REQUIRED_CHANNELS = ["@your channel username"]
-GROUP_JOIN_LINK = "https://t.me/your_group_link"
-OWNER_ID = your tg user id (integer)    #Example: 6282811167
-OWNER_USERNAME = "@your username"
-
+REQUIRED_CHANNELS = ["@SAND LIKE 🔍"]
+GROUP_JOIN_LINK = "https://t.me/sanwwu12bot
+OWNER_ID =  8261853385
+OWNER_USERNAME = "@sand2187 username"
 bot = telebot.TeleBot(BOT_TOKEN)
 like_tracker = {}   # in-memory cache
 
